a1b96fa6f767ef9504e621dfc0899b7e564a916d
Felipe Fabossi <fabossif@gmail.com>
2020-07-24 19:29:44 -0300

    [FABOSSI] Project design ready

--- Desktop/workspace/Felipe-Fabssi-Projects/Angular-Projects/designes/nike-shop/src/app/app.module.ts
+++ Desktop/workspace/Felipe-Fabssi-Projects/Angular-Projects/designes/nike-shop/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { LeftBarComponent } from './components/left-bar/left-bar.component';
 import { MiddleBarComponent } from './components/middle-bar/middle-bar.component';
 import { RightBarComponent } from './components/right-bar/right-bar.component';
 import { SearchBuyComponent } from './components/right-bar/search-buy/search-buy.component';
+import { PriceComponent } from './components/middle-bar/price/price.component';
+import { MiddleContainerComponent } from './components/middle-bar/middle-container/middle-container.component';
+import { ProductInfoComponent } from './components/middle-bar/product-info/product-info.component';
+import { RatingBarComponent } from './components/middle-bar/rating-bar/rating-bar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { SearchBuyComponent } from './components/right-bar/search-buy/search-buy
     LeftBarComponent,
     MiddleBarComponent,
     RightBarComponent,
-    SearchBuyComponent
+    SearchBuyComponent,
+    PriceComponent,
+    MiddleContainerComponent,
+    ProductInfoComponent,
+    RatingBarComponent
   ],
   imports: [
     BrowserModule,
